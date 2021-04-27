7f909ba33cc45e2e97eed4606b1021e1aec53229
Antonio Papa <antoniopapa1991@gmail.com>
2021-04-27 11:06:25 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ['http://localhost:4200', 'http://localhost:3000', 'http://localhost:8080']
+    })
+    await app.listen(8000);
 }
+
 bootstrap();
